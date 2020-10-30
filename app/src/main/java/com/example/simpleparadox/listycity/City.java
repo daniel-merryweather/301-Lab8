3573925ef2054b0b94d6b0818a9677055417815a
daniel-merryweather <63621594+daniel-merryweather@users.noreply.github.com>
2020-10-29 18:24:57 -0600

    Added isCapital feature

--- app/src/main/java/com/example/simpleparadox/listycity/City.java
+++ app/src/main/java/com/example/simpleparadox/listycity/City.java
@@ -3,10 +3,18 @@ package com.example.simpleparadox.listycity;
 public class City implements Comparable<City>{
     private String city;
     private String province;
+    private boolean isCaptial;
 
     City(String city, String province){
         this.city = city;
         this.province = province;
+        this.isCaptial = false;
+    }
+    
+    City(String city, String province, boolean isCaptial){
+        this.city = city;
+        this.province = province;
+        this.isCaptial = isCaptial;
     }
 
     String getCityName(){
@@ -15,6 +23,10 @@ public class City implements Comparable<City>{
 
     String getProvinceName(){
         return this.province;
+    }
+    
+    boolean getIsCaptial(){
+        return this.isCaptial;
     }
 
     @Override
