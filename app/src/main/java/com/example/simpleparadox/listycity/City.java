9fe6c19822915107a0908acc27684259bd599a04
daniel-merryweather <63621594+daniel-merryweather@users.noreply.github.com>
2020-10-29 18:30:04 -0600

    Fixed feature implementation

--- app/src/main/java/com/example/simpleparadox/listycity/City.java
+++ app/src/main/java/com/example/simpleparadox/listycity/City.java
@@ -3,18 +3,18 @@ package com.example.simpleparadox.listycity;
 public class City implements Comparable<City>{
     private String city;
     private String province;
-    private boolean isCaptial;
+    private boolean isCapital;
 
     City(String city, String province){
         this.city = city;
         this.province = province;
-        this.isCaptial = false;
+        this.isCapital = false;
     }
     
-    City(String city, String province, boolean isCaptial){
+    City(String city, String province, boolean isCapital){
         this.city = city;
         this.province = province;
-        this.isCaptial = isCaptial;
+        this.isCapital = isCapital;
     }
 
     String getCityName(){
@@ -25,8 +25,8 @@ public class City implements Comparable<City>{
         return this.province;
     }
     
-    boolean getIsCaptial(){
-        return this.isCaptial;
+    boolean getIsCapital(){
+        return this.isCapital;
     }
 
     @Override
