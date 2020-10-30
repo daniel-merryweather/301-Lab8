1ceff2a096b00b375843f446447e45767741c33c
daniel-merryweather <63621594+daniel-merryweather@users.noreply.github.com>
2020-10-29 18:27:53 -0600

    Added test for isCapital feature

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -28,4 +28,15 @@ class CityListTest {
         assertEquals(2, cityList.countCities());
         assertTrue(cityList.hasCity(new City("Regina", "Saskatchewan")));
     }
+    
+    @Test
+    void testIsCapital(){
+        City city = mockCity();
+        
+        assertEquals(false, city.getIsCapital());
+        
+        City capitalCity = new City("Edmonton", "Alberta", true);
+        
+        assertEquals(true, city.getIsCapital());
+    }
 }
